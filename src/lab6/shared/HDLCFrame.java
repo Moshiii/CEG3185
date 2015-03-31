b7909616d3f4411a41c1fb76f8400b26578db2b2
pliu037 <pengliu12@gmail.com>
2015-03-31 01:59:20 -0400

    Lab 6
    Completed implementation of HDLC frames

--- src/lab6/shared/HDLCFrame.java
+++ src/lab6/shared/HDLCFrame.java
@@ -1,18 +1,39 @@
 package lab6.shared;
 
+import java.util.StringTokenizer;
+
 /**
  * Created by Peng on 30/03/2015.
  */
 public class HDLCFrame {
 
     private static final String FLAG = "01111110";
-    public static final String VALID_FRAME_TYPES = "IS";
+    private static final String I_PREFIX = "0";
+    private static final String S_PREFIX = "10";
+    private static final String U_PREFIX = "11";
+    private static final String SEPARATOR = ".";
+    private static final int CONTROL_LENGTH = 8;
+    private static final int TYPE_LENGTH = 2;
+    private static final int MINIMUM_ADDRESS_LENGTH = 8;
+    public static final String VALID_FRAME_TYPES = "ISU";
     public static final String RR = "00";
     public static final String RNR = "01";
     public static final String REJ = "10";
     public static final String SREJ = "11";
-    public static final int ADDRESS_LENGTH = 8;
+    public static final String SNRM = "00001";
+    public static final String SNRME = "11011";
+    public static final String SABM_DM = "11100";
+    public static final String SABME = "11110";
+    public static final String UI = "00000";
+    public static final String UA = "00110";
+    public static final String DISC_RD = "00010";
+    public static final String SIM_RIM = "10000";
+    public static final String UP = "00100";
+    public static final String RSET = "11001";
+    public static final String XID = "11101";
+    public static final String FRMR = "10001";
     public static final int WINDOW_LENGTH = 3;
+    public static final int MAX_DATA_LENGTH = 64*8;
 
     private final int address;
     private final int seqRcv;
@@ -33,26 +54,6 @@ public class HDLCFrame {
         this.data = data;
     }
 
-    public static HDLCFrameBuilder getHDLCFrameBuilder(int address, int seqRcv, boolean poll, char frameType) {
-        return HDLCFrameBuilder.getInstance(address, seqRcv, poll, frameType);
-    }
-
-    public static HDLCFrame valueOf(String frame) {
-        if (!frame.substring(0, 8).equals(FLAG)) {
-            System.out.println("Frame flag is incorrect so the frame will be dropped.");
-            return null;
-        }
-        int address = 0;
-        int seqRcv = 0;
-        boolean poll = true;
-        char frameType = 'I';
-        int seqSend = 0;
-        String type = "00";
-        String data = "Whatever";
-        HDLCFrameBuilder builder = HDLCFrame.getHDLCFrameBuilder(address, seqRcv, poll, frameType);
-        return builder.getHDLCFrame();
-    }
-
     private static String toBinaryString(int number, int length) {
         String bin = Integer.toBinaryString(number);
         while(bin.length() < length) {
@@ -61,21 +62,192 @@ public class HDLCFrame {
         return bin;
     }
 
+    private static boolean isValidBinary(String check) {
+        for (int i = 0; i < check.length(); i ++) {
+            if ("01".indexOf(check.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static HDLCFrameBuilder getHDLCFrameBuilder(int address, boolean poll, char frameType) {
+        return HDLCFrameBuilder.getInstance(address, poll, frameType);
+    }
+
+    public static HDLCFrame valueOf(String frame) {
+        if (!frame.substring(0, FLAG.length()).equals(FLAG)) {
+            System.out.println("Start flag is missing. Dropping frame.");
+            return null;
+        }
+        frame = frame.substring(FLAG.length());
+
+        StringTokenizer tokenizer = new StringTokenizer(frame, SEPARATOR);
+        if (!tokenizer.hasMoreTokens()) {
+            System.out.println("Missing SEPARATOR. Dropping frame.");
+            return null;
+        }
+        String check = tokenizer.nextToken();
+        if (check.length() < MINIMUM_ADDRESS_LENGTH || !isValidBinary(check)) {
+            System.out.println("Address field is corrupted. Dropping frame.");
+            return null;
+        }
+        int address = Integer.parseInt(check, 2);
+        frame = tokenizer.nextToken();
+
+        if (frame.length() < CONTROL_LENGTH + FLAG.length()) {
+            System.out.println("Frame is too short. Dropping frame.");
+            return null;
+        }
+        char frameType;
+        if (frame.charAt(0) == '0') {
+            frameType = 'I';
+            frame = frame.substring(1);
+        }
+        else if (frame.charAt(0) == '1') {
+            if (frame.charAt(1) == '0') {
+                frameType = 'S';
+            }
+            else if (frame.charAt(1) == '1') {
+                frameType = 'U';
+            }
+            else {
+                System.out.println("Frame type is corrupted. Dropping frame.");
+                return null;
+            }
+            frame = frame.substring(TYPE_LENGTH);
+        }
+        else {
+            System.out.println("Frame type is corrupted. Dropping frame.");
+            return null;
+        }
+
+        int seqSend = -1;
+        String type = null;
+        if (frameType == 'I') {
+            check = frame.substring(0, WINDOW_LENGTH);
+            if (!isValidBinary(check)) {
+                System.out.println("Send sequence is corrupted. Dropping frame.");
+                return null;
+            }
+            seqSend = Integer.parseInt(check, 2);
+            frame = frame.substring(WINDOW_LENGTH);
+        }
+        else if (frameType == 'S' || frameType == 'U') {
+            type = frame.substring(0, 2);
+            if (!isValidBinary(type)) {
+                System.out.println("Type is corrupted. Dropping frame.");
+                return null;
+            }
+            frame = frame.substring(TYPE_LENGTH);
+        }
+
+        boolean poll;
+        if (frame.charAt(0) == '1') {
+            poll = true;
+        }
+        else if (frame.charAt(0) == '0') {
+            poll = false;
+        }
+        else {
+            System.out.println("Poll bit is corrupted. Dropping frame.");
+            return null;
+        }
+        frame = frame.substring(1);
+
+        int seqRcv = -1;
+        check = frame.substring(0, WINDOW_LENGTH);
+        if (isValidBinary(check)) {
+            if (frameType == 'I' || frameType == 'S') {
+                seqRcv = Integer.parseInt(check, 2);
+            }
+            else if (frameType == 'U') {
+                type = type + check;
+            }
+        }
+        else {
+            System.out.println("Send sequence or type is corrupted. Dropping frame.");
+            return null;
+        }
+        frame = frame.substring(WINDOW_LENGTH);
+
+        int index = frame.indexOf(FLAG);
+        if (index == -1) {
+            System.out.println("End flag is missing. Dropping frame.");
+            return null;
+        }
+        String data = frame.substring(0, index);
+        if (frameType == 'S' || frameType == 'U') {
+            if (data.length() > 0) {
+                System.out.println("S-frames and U-frames shouldn't contain data. Dropping frame.");
+                return null;
+            }
+        }
+        else if (frameType == 'I') {
+            if (data.length() > MAX_DATA_LENGTH) {
+                System.out.println("Maximum data length exceeded. Dropping frame.");
+                return null;
+            }
+        }
+
+        HDLCFrameBuilder builder = HDLCFrame.getHDLCFrameBuilder(address, poll, frameType);
+        if (builder == null) {
+            return null;
+        }
+
+        if (frameType == 'I') {
+            builder.setData(data);
+            builder.setSeqRcv(seqRcv);
+            builder.setSeqSend(seqSend);
+        }
+        else if (frameType == 'S') {
+            builder.setSeqRcv(seqRcv);
+            builder.setType(type);
+        }
+        else if (frameType == 'U') {
+            builder.setType(type);
+        }
+
+        return builder.getHDLCFrame();
+    }
+
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append(FLAG);
-        builder.append(toBinaryString(address, ADDRESS_LENGTH));
+        builder.append(toBinaryString(address, MINIMUM_ADDRESS_LENGTH));
+        builder.append(SEPARATOR);
+
         if (frameType == 'I') {
-            builder.append("0");
+            builder.append(I_PREFIX);
             builder.append(toBinaryString(seqSend, WINDOW_LENGTH));
         }
-        else {
-            builder.append("10");
+        else if (frameType == 'S') {
+            builder.append(S_PREFIX);
             builder.append(type);
         }
+        else if (frameType == 'U') {
+            builder.append(U_PREFIX);
+            builder.append(type.substring(0, TYPE_LENGTH));
+        }
+
         if (poll) {
             builder.append("1");
         }
+        else {
+            builder.append("0");
+        }
+
+        if (frameType == 'I' || frameType == 'S') {
+            builder.append(toBinaryString(seqRcv, WINDOW_LENGTH));
+        }
+        else if (frameType == 'U') {
+            builder.append(type.substring(TYPE_LENGTH, TYPE_LENGTH + WINDOW_LENGTH));
+        }
+
+        if (frameType == 'I') {
+            builder.append(data);
+        }
+
         builder.append(FLAG);
         return builder.toString();
     }
@@ -108,7 +280,11 @@ public class HDLCFrame {
         return data;
     }
 
+    //Running some simple tests
     public static void main (String[] args) {
-        System.out.println(HDLCFrame.toBinaryString(10, 6));
+        String strng = "0111111000000000.1100000001111110";
+        HDLCFrame test = HDLCFrame.valueOf(strng);
+        System.out.println(test.toString());
+        System.out.println(strng);
     }
 }
--- src/lab6/shared/HDLCFrameBuilder.java
+++ src/lab6/shared/HDLCFrameBuilder.java
@@ -5,35 +5,35 @@ package lab6.shared;
  */
 public class HDLCFrameBuilder {
 
-    private static final int MAX_ADDRESS = (int)Math.pow(2, HDLCFrame.ADDRESS_LENGTH) - 1;
     private static final int MAX_WINDOW = (int)Math.pow(2, HDLCFrame.WINDOW_LENGTH) - 1;
 
     private int address;
-    private int seqRcv;
+    private int seqRcv = -1;
     private boolean poll;
     private char frameType;
-    private int seqSend;
-    private String type;
-    private String data;
+    private int seqSend = -1;
+    private String data = null;
+    private String type = null;
 
-    private HDLCFrameBuilder(int address, int seqRcv, boolean poll, char frameType) {
+    private HDLCFrameBuilder(int address, boolean poll, char frameType) {
         this.address = address;
-        this.seqRcv = seqRcv;
         this.poll = poll;
         this.frameType = frameType;
-        if (frameType == 'I') {
-            type = null;
-        }
-        else if (frameType == 'S') {
-            seqSend = -1;
-        }
     }
 
     private static boolean validAddress(int address) {
-        if (address >= 0 && address <= MAX_ADDRESS) {
+        if (address >= 0) {
             return true;
         }
-        System.out.println("Invalid address (must be between 0 and " + MAX_ADDRESS + ", inclusive): " + address);
+        System.out.println("Invalid address (must be non-negative): " + address);
+        return false;
+    }
+
+    private static boolean validFrameType(char frameType) {
+        if (HDLCFrame.VALID_FRAME_TYPES.indexOf(frameType) != -1) {
+            return true;
+        }
+        System.out.println("Invalid frame type (must be 'I', 'S', or 'U'): " + frameType);
         return false;
     }
 
@@ -45,50 +45,132 @@ public class HDLCFrameBuilder {
         return false;
     }
 
-    private static boolean validFrameType(char frameType) {
-        if (HDLCFrame.VALID_FRAME_TYPES.indexOf(frameType) != -1) {
+    private static boolean isValidSType(String type) {
+        if (type.equals(HDLCFrame.RR) || type.equals(HDLCFrame.RNR) || type.equals(HDLCFrame.REJ)
+                || type.equals(HDLCFrame.SREJ)) {
             return true;
         }
-        System.out.println("Invalid frame type (must be 'I' or 'S'): " + frameType);
         return false;
     }
 
-    private static boolean isValid(int address, int seqRcv, boolean poll, char frameType) {
+    private static boolean isValidUType(String type) {
+        if (type.equals(HDLCFrame.SNRM) || type.equals(HDLCFrame.SNRME) || type.equals(HDLCFrame.SABM_DM)
+                || type.equals(HDLCFrame.SABME) || type.equals(HDLCFrame.UI) || type.equals(HDLCFrame.UA)
+                || type.equals(HDLCFrame.DISC_RD) || type.equals(HDLCFrame.SIM_RIM) || type.equals(HDLCFrame.UP)
+                || type.equals(HDLCFrame.RSET) || type.equals(HDLCFrame.XID) || type.equals(HDLCFrame.FRMR)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isValid(int address, boolean poll, char frameType) {
         boolean flag = true;
+
         if (!validAddress(address)) {
             flag = false;
         }
-        if (!validSeq(seqRcv)) {
-            flag = false;
-        }
+
         if (!validFrameType(frameType)) {
             flag = false;
         }
+
         return flag;
     }
 
-    public static HDLCFrameBuilder getInstance(int address, int seqRcv, boolean poll, char frameType) {
-        if (isValid(address, seqRcv, poll, frameType)) {
-            return new HDLCFrameBuilder(address, seqRcv, poll, frameType);
+    public static HDLCFrameBuilder getInstance(int address, boolean poll, char frameType) {
+        if (isValid(address, poll, frameType)) {
+            return new HDLCFrameBuilder(address, poll, frameType);
         }
         return null;
     }
 
-    public void putSeqSend(int seqSend) {
+    public void setSeqRcv(int seqRcv) {
+        if ((frameType == 'I' || frameType == 'S') && validSeq(seqRcv)) {
+            this.seqRcv = seqRcv;
+        }
+        else {
+            System.out.println("U-frames do not contain a receive sequence!");
+        }
+    }
+
+    public void setSeqSend(int seqSend) {
         if (frameType == 'I' && validSeq(seqSend)) {
             this.seqSend = seqSend;
         }
         else {
-            System.out.println("S-frames do not contain a send sequence!");
+            System.out.println("S-frames and U-frames do not contain a send sequence!");
         }
     }
 
-    public void putData (String data) {
-        this.data = data;
+    public void setData (String data) {
+        if (frameType == 'I') {
+            if (data != null && data.length() <= HDLCFrame.MAX_DATA_LENGTH) {
+                this.data = data;
+            }
+            else {
+                System.out.println("Invalid data!");
+            }
+        }
+        else {
+            System.out.println("S-frames and U-frames do not contain data!");
+        }
     }
 
-    public boolean isValid() {
+    public void setType (String type) {
+        if (frameType == 'S') {
+            if (type != null && isValidSType(type)) {
+                this.type = type;
+            }
+            else {
+                System.out.println("Invalid S-frame type!");
+            }
+        }
+        else if (frameType == 'U') {
+            if (type != null && isValidUType(type)) {
+                this.type = type;
+            }
+            else {
+                System.out.println("Invalid U-frame type!");
+            }
+        }
+        else {
+            System.out.println("I-frames do not contain a type!");
+        }
+    }
+
+    private boolean isValid() {
         boolean flag = true;
+
+        if (frameType == 'I') {
+            if (seqRcv == -1) {
+                System.out.println("Receive sequence not set!");
+                flag = false;
+            }
+            if (seqSend == -1) {
+                System.out.println("Send sequence not set!");
+                flag = false;
+            }
+            if (data == null) {
+                System.out.println("Data not set!");
+                flag = false;
+            }
+        }
+        else if (frameType == 'S') {
+            if (seqRcv == -1) {
+                System.out.println("Receive sequence not set!");
+                flag = false;
+            }
+            if (type == null) {
+                System.out.println("Type not set!");
+                return false;
+            }
+        }
+        else if (frameType == 'U') {
+            if (type == null) {
+                System.out.println("Type not set!");
+                return false;
+            }
+        }
 
         return flag;
     }
